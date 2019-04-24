86d06b63f4d63c0f10c80ea6a1d4fc24d816fbd1
Lucas Messenger <lucas.c.messenger@gmail.com>
2019-04-23 23:53:33 -0700

    Add utilities for better git, vim, and setting up first time macOS

---
+++ setup_mac.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# TODO add --personal flag to include additional packages
+
+# """ setup homebrew """
+# install homebrew
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+
+# install packages from homebrew
+brew install cloc curl git go grep htop iproute2mac irssi jq md5sha1sum nmap \
+             openssh openssl python python@2 python3 rsync screen sshrc \
+             sqlite telnet visidata watch wget wireguard-tools xz
+
+# install packages from homebrew cask
+brew install caskroom/cask/brew-cask
+brew cask install atom docker dropbox flux firefox google-chrome keepingyouawake \
+                  iterm2 postman the-unarchiver virtualbox vlc wireshark --force
+
+# cleanup
+brew cleanup
+
+# future cask ideas
+# brew cask install airpass nightowl there
+
+# """ setup bash """
+echo "alias ll='ls -la'" >> ~/.bash_profile
+echo "weather() { curl wttr.in/\"$@\"; }" >> ~/.bash_profile
+echo "alias g='git'" >> ~/.bash_profile
+echo "alias gs='git status '" >> ~/.bash_profile
+echo "alias ga='git add '" >> ~/.bash_profile
+echo "alias gb='git branch '" >> ~/.bash_profile
+echo "alias gc='git commit'" >> ~/.bash_profile
+echo "alias gd='git diff'" >> ~/.bash_profile
+echo "alias gco='git checkout '" >> ~/.bash_profile
+
+# """ setup vim """
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/undo
+cp vimrc ~/.vimrc
--- vimrc
+++ vimrc
@@ -1,23 +1,30 @@
 set nocompatible
-set number
 filetype off
+set number
 
-"set rtp+=~/.vim/bundle/Vundle.vim
-"call vundle#begin()
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
 
-"Plugin 'VundleVim/Vundle.vim'
+Plugin 'VundleVim/Vundle.vim'
 
 syntax on
-set tabstop=4 shiftwidth=4
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
 set expandtab
 set autoindent
 set smartindent
 set backspace=indent,eol,start
+set encoding=utf-8
 
 noremap Y y$
 set list listchars=tab:▷⋅,trail:⋅,nbsp:⋅
-"autoremove trailing whitespace
-autocmd BufWritePre * call SetSpaces()
+
+au BufWritePre *.py,*.rb,*.go,*.sh,*.txt retab
+au BufWritePre * call SetSpaces()
+au BufNewFile,BufRead *.py
+    \ set textwidth=79
+    \ set fileformat=linux
 
 " Searching
 set ignorecase
@@ -25,7 +32,16 @@ set smartcase
 set hlsearch
 set incsearch
 
+" File management
+set noswapfile
+set nobackup
+set nowritebackup
+set cursorline
+
+" Allow for undoing changes after exit and reopen
+set undofile
+set undodir=~/.vim/undo/
+
 fun SetSpaces()
-    retab
     %s/\s\+$//e
 endfun
