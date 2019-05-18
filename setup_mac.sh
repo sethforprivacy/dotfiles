78db1b786a01d924e52e93d3dedd17eb4881d91d
Lucas Messenger <lucas.c.messenger@gmail.com>
2019-05-17 17:48:24 -0700

    Add vim, tmux. Bash_profile as file and setup.

--- setup_mac.sh
+++ setup_mac.sh
@@ -2,6 +2,10 @@
 # TODO add --personal flag to include additional packages
 # NOTE future cask ideas: airpass nightowl there
 
+# """ setup bash """
+cp ~/.bash_profile ~/.bash_profile.bak
+cp bash_profile ~/.bash_profile
+
 # """ setup homebrew """
 # install homebrew
 xcode-select --install
@@ -11,34 +15,24 @@ brew doctor
 # install packages from homebrew
 brew install cloc curl git go grep htop iproute2mac irssi jq md5sha1sum nmap \
              openssh openssl python python@2 python3 rsync screen sshrc \
-             sqlite telnet watch wget xz
+             sqlite telnet tmux vim watch wget xz
 
 # install packages from homebrew cask
 brew install caskroom/cask/brew-cask
 brew cask install atom docker dropbox flux firefox google-chrome gpg-suite keepingyouawake \
-                  iterm2 postman the-unarchiver virtualbox vlc wireshark --force
+                  iterm2 postman the-unarchiver virtualbox vlc wireshark whatsapp --force
 
 # cleanup
 brew cleanup
 
+# """ setup git """
+cp gitconfig ~/.gitconfig
+
 # """ install visidata """
 pip3 install visidata dpkt lxml
-
-# """ setup bash """
-echo "alias ll='ls -la'" >> ~/.bash_profile
-echo "weather() { curl wttr.in/\"$@\"; }" >> ~/.bash_profile
-echo "alias g='git'" >> ~/.bash_profile
-echo "alias gs='git status '" >> ~/.bash_profile
-echo "alias ga='git add '" >> ~/.bash_profile
-echo "alias gb='git branch '" >> ~/.bash_profile
-echo "alias gc='git commit'" >> ~/.bash_profile
-echo "alias gd='git diff'" >> ~/.bash_profile
-echo "alias gco='git checkout '" >> ~/.bash_profile
 
 # """ setup vim """
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/undo
 cp vimrc ~/.vimrc
 
-# """ setup git """
-cp gitconfig ~/.gitconfig
