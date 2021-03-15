6c5376997f7643c8cf321e28090788d3ee38972f
Seth Simmons <40500387+sethsimmons@users.noreply.github.com>
2021-03-15 16:21:08 -0400

    Update to my own personal packages and zshrc

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# TODO add --personal flag to include additional packages
-# NOTE future cask ideas: airpass nightowl there
-
-# """ setup bash """
-cp ~/.bash_profile ~/.bash_profile.bak
-cp bash_profile ~/.bash_profile
 
 # """ setup homebrew """
 # install homebrew
@@ -13,26 +7,35 @@ xcode-select --install
 brew doctor
 
 # install packages from homebrew
-brew install ansible ansible-lint cloc curl git go grep htop iproute2mac \
-             jq md5sha1sum nmap openssh openssl python3 rsync screen sshrc \
-             inetutils tmux vim watch wget xz zstd zsh
+brew install ansible ansible-lint autojump curl git go grep htop iftop \
+             jq md5sha1sum nmap openssh openssl python3 sshrc \
+             inetutils starship vim watch wget xz zstd zsh zsh-autosuggestions zsh-syntax-highlighting
 
 # install packages from homebrew cask
 brew install caskroom/cask/brew-cask
-brew cask install visual-studio-code docker flux firefox gpg-suite keepassxc keepingyouawake \
-                  iterm2 postman the-unarchiver tresorit virtualbox vlc wireshark signal --force
+brew cask install brave-browser bitwarden docker element gpg-suite keepingyouawake \
+                  iterm2 signal spotify virtualbox visual-studio-code vlc wireshark --force
 
 # cleanup
 brew cleanup
 
+# Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Add .zshrc configurations
+echo '''alias iftop='sudo iftop -c ~/.iftoprc'
+export PATH="/usr/local/sbin:$PATH"
+eval "$(starship init zsh)"''' | tee -a ~/.zshrc
+
+# Enable zsh plugins
+sed "s/plugins=*/plugins=(git autojump brew vagrant zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
+source ~/.zshrc
+
 # install xcode-select
 xcode-select --install
-
-# """ setup git """
-cp gitconfig ~/.gitconfig
 
 # """ install visidata """
 pip3 install visidata dpkt lxml virtualenv
 
-# """ setup vim """
-./setup_vim.sh
+# Setup iftoprc
+cp .iftoprc ~/.iftoprc
